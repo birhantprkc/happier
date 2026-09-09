f42a50784de856f25f6ffee70411f8ec61cc6afd
Leeroy Brun <leeroy.brun@gmail.com>
2026-09-09 06:50:29 +0200

    fix(stack): retry Windows runtime cleanup

--- packages/cli-common/src/firstPartyRuntime/index.ts
+++ packages/cli-common/src/firstPartyRuntime/index.ts
@@ -68,6 +68,7 @@ export type {
   ReleaseAssetBundle,
 } from './releaseAssetBundle.js';
 export { extractReleasePayloadRootFromArchive } from './extractReleasePayloadRootFromArchive.js';
+export { removeRuntimePayloadPath } from './copyRuntimePayloadTree.js';
 
 export { listInstalledVersionIdsNewestFirst } from './listInstalledVersionIdsNewestFirst.js';
 export { installVersionedPayload } from './installVersionedPayload.js';
